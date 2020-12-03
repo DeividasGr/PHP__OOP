a83b6b27231aa5d2d247783eeecba35abadc3e38
Gruzinas1 <dgruzdzevicius@gmail.com>
2020-12-03 12:48:58 +0200

    html and index templates created, BasePage class created

--- public_html/index.php
+++ public_html/index.php
@@ -1,7 +1,9 @@
 <?php
 
 use App\app;
+use App\Views\BasePage;
 use App\Views\Navigation;
+use Core\View;
 
 require '../bootloader.php';
 
@@ -9,30 +11,15 @@ require '../bootloader.php';
 $my_pixels = App::$db->getRowsWhere('pixels');
 $nav = new Navigation();
 
-?>
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="/media/style.css">
-    <title>Shit Wall</title>
-</head>
-<body>
-<?php print $nav->render(); ?>
-<section class="box">
+$content = new View([
+    'title' => 'Poop-Wall',
+    'products' => App::$db->getRowsWhere('pixels')
+]);
 
-    <div class="wall">
-        <?php foreach ($my_pixels as $span): ?>
-            <span class="pixels" style="
-                    background: <?php print $span['colors']; ?>;
-                    top: <?php print $span['X']; ?>px;
-                    left: <?php print $span['Y']; ?>px; ">
-            </span>
-        <?php endforeach; ?>
-    </div>
-</section>
-</body>
-</html>
+$page = new BasePage([
+    'title' => 'Index',
+    'content' => $content->render(ROOT . '/app/templates/content/index.tpl.php')
+]);
+
+print $page->render();
+?>
